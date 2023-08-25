8f5c507b963df7cde4534422143433db5ac25ef3
fvckprth <parth@qortor.com>
2023-08-25 17:42:35 -0400

    scrolling issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen overflow-y-hidden">
+    <div className="bg-black h-full overflow-y-hidden">
       <ThreeDots />
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 md:px-0 w-full md:w-auto">
-        <div className="max-w-screen-sm mx-auto md:px-0">
+        <div className="max-w-full-sm mx-auto md:px-0">
           <WaitlistForm />
           <Footer />
         </div>
