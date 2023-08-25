5f77ad7ab274c044a0f0968dd974d779938e8bd5
fvckprth <parth@qortor.com>
2023-08-25 17:20:15 -0400

    removed bg temp

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <div className="bg-black h-screen">
       <ThreeDots />
-      <BlurImages />
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 px-6 md:px-0 w-full md:w-auto">
         <div className="max-w-screen-sm mx-auto md:px-0">
           <WaitlistForm />
+          <Footer />
         </div>
       </div>
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,21 @@ import { motion, useAnimation } from 'framer-motion';
 
 function Footer() {
     return (
-        <div className='flex flex-col justify-between items-center'>
+        <motion.div
+            initial={{ opacity: 0, y: 5, filter: "blur(5px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+            transition={{ delay: 1.2, duration: 0.9 }}
+        >
+        <div className='flex flex-row mt-6 justify-between items-start'>
             <div>
-                <p>© 2023 East Park Holdings Group</p>
-                <p>info@eastpark.xyz</p>
+                <p className='text-sm text-[#5D5D5D] text-opacity-25 leading-tight'>
+                    © 2023 East Park Holdings Group<br />
+                    info@eastpark.xyz
+                </p>
             </div>
-            <Image src="/images/p307-asset.svg" alt="P307 Brand Asset" layout="fill" objectFit="cover" />
+            <img src="/images/p307-asset.svg" alt="P307 Logo" className="h-6 md:h-8 w-auto" /> {/* Adjust size as needed */}
         </div>
+        </motion.div>
     )
 }
 
