b5ee520338185ba3f4b7cd0ef8d29c96203d1ad9
fvckprth <parth@qortor.com>
2023-08-25 17:47:30 -0400

    Update ThreeDots.tsx

--- components/ThreeDots.tsx
+++ components/ThreeDots.tsx
@@ -10,7 +10,7 @@ function ThreeDots() {
     animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
     transition={{ delay: 0.3, duration: 0.9 }}
     >
-    <div className="flex flex-col items-center justify-between h-screen">
+    <div className="flex flex-col items-center justify-between h-full">
       <div className="flex justify-between w-full">
         <div style={{ backgroundColor: '#CBADFB' }} className="md:w-12 md:h-12 w-6 h-6"></div>
         <div style={{ backgroundColor: '#ADFBF6' }} className="md:w-12 md:h-12 w-6 h-6"></div>
