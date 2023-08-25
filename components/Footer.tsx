aea895e19a64e8dc49bd3ba40c46c725d645c1a4
fvckprth <parth@qortor.com>
2023-08-25 17:37:54 -0400

    scroll issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,14 +9,14 @@ function Footer() {
             animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
             transition={{ delay: 1.2, duration: 0.9 }}
         >
-        <div className='flex flex-row mt-6 justify-between items-start'>
+        <div className='flex flex-row mt-6 justify-between items-end'>
             <div>
                 <p className='text-xs md:text-sm text-[#5D5D5D] text-opacity-25 leading-tight tracking-tight'>
                     © 2023 East Park Holdings Group<br />
                     info@eastpark.xyz
                 </p>
             </div>
-            <img src="/images/p307-asset.svg" alt="P307 Logo" className="h-6 md:h-8 w-auto" /> {/* Adjust size as needed */}
+            <img src="/images/p307-asset.svg" alt="P307 Logo" className="h-6 md:h-10 w-auto" /> {/* Adjust size as needed */}
         </div>
         </motion.div>
     )
