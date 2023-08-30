66e5ef00183c8fdc9e3e62f24745843f67984ada
fvckprth <parth@qortor.com>
2023-08-29 21:48:19 -0400

    layout.tsx update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const fkGrotesk = localFont({
 });
 
 export const metadata = {
-  title: 'P307 ⎯ Let anyone sell your products anywhere.',
+  title: 'Let anyone sell your products anywhere.',
   description: 'Enable your community to market and sell your products anywhere on the internet.',
   siteUrl: 'https://www.p307.net',
   twitterHandle: '@p307',
@@ -21,6 +21,8 @@ type RootLayoutProps = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const imageUrl = `${metadata.siteUrl}/public/images/P307-social.png`;
+  
   return (
     <html lang="en">
       <Head>
@@ -28,6 +30,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0 viewport-fit=cover" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.siteUrl} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={imageUrl} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={metadata.siteUrl} />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta property="twitter:image" content={imageUrl} />
       </Head>
       <body className={fkGrotesk.className}>{children}</body>
     </html>
