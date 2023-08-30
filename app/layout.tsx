534dc8a253a3d8c1647b0adee5449df9f16ca42a
fvckprth <parth@qortor.com>
2023-08-30 17:36:32 -0400

    Updated image layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta property="og:url" content={metadata.siteUrl} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
+        <meta property="og:image" content="https://p307.net/images/P307-social.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="675" />
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta property="twitter:url" content={metadata.siteUrl} />
         <meta property="twitter:title" content={metadata.title} />
         <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content={metadata.image} />
+        <meta property="twitter:image" content="https://p307.net/images/P307-social.png" />
         <meta property="twitter:type" content="image/png" />
         <meta property="twitter:width" content="1200" />
         <meta property="twitter:height" content="675" />
